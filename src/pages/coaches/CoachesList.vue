02f0258a28a3b3a1155d00e98a3a4c66848e3d9c
rahulkotla2 <rahulkotla2@gmail.com>
2023-02-17 10:52:08 +0530

    Configured CoachItem component

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -8,14 +8,19 @@
             <router-link to="/register">Register as Coach</router-link>
         </div>
         <ul v-if="hasCoaches">
-            <li v-for="coach in coaches" :key="coach.id">{{ coach.firstName }}</li>
+            <coach-item v-for="coach in coaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
+                :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
         </ul>
         <h2 v-else>No Coaches Available</h2>
-    </section>
+</section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
 export default {
+    components: {
+        CoachItem,
+    },
     computed: {
         coaches() {
             return this.$store.getters['coaches/coaches'];
@@ -40,5 +45,16 @@ html {
 
 body {
     margin: 0;
+}
+
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+.controls {
+    display: flex;
+    justify-content: space-between;
 }
 </style>
