db9da7704f26310436163d18ae9393b3cf405910
rahulkotla2 <rahulkotla2@gmail.com>
2023-02-17 11:06:38 +0530

    Creation and Addition of BaseCard Component

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,15 +3,17 @@
         FILTER
     </section>
     <section>
-        <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
-        </div>
-        <ul v-if="hasCoaches">
-            <coach-item v-for="coach in coaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
-                :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
-        </ul>
-        <h2 v-else>No Coaches Available</h2>
+        <base-card>
+            <div class="controls">
+                <button>Refresh</button>
+                <router-link to="/register">Register as Coach</router-link>
+            </div>
+            <ul v-if="hasCoaches">
+                <coach-item v-for="coach in coaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
+                    :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
+            </ul>
+            <h2 v-else>No Coaches Available</h2>
+        </base-card>
 </section>
 </template>
 
