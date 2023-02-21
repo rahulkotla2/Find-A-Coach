b1462787b2d91c2ad6367c1088ae374e63cc2130
rahulkotla2 <rahulkotla2@gmail.com>
2023-02-21 22:51:09 +0530

    Added Base spinner component for loading

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,9 +6,12 @@
         <base-card>
             <div class="controls">
                 <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
-                <base-button v-if="!isCoach" link to="/register">Register as Coach</base-button>
+                <base-button v-if="!isCoach && !isLoading" link to="/register">Register as Coach</base-button>
             </div>
-            <ul v-if="hasCoaches">
+            <div v-if="isLoading">
+                <base-spinner></base-spinner>
+            </div>
+            <ul v-else-if="hasCoaches">
                 <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
                     :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
             </ul>
@@ -23,6 +26,8 @@ import CoachFilter from '../../components/coaches/CoachFilters.vue';
 export default {
     data() {
         return {
+            isLoading: false,
+            error: null,
             activeFilters: {
                 frontend: true,
                 backend: true,
@@ -54,7 +59,7 @@ export default {
             })
         },
         hasCoaches() {
-            return this.$store.getters['coaches/hasCoaches'];
+            return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
         }
     },
     created() {
@@ -64,8 +69,14 @@ export default {
         setFilters(updatedFilters) {
             this.activeFilters = updatedFilters;
         },
-        loadCoaches() {
-            this.$store.dispatch('coaches/loadCoaches');
+        async loadCoaches() {
+            this.isLoading = true;
+            try {
+                await this.$store.dispatch('coaches/loadCoaches');
+            } catch (error) {
+                this.error = error.message || 'Something went wrong!';
+            }
+            this.isLoading = false;
         }
     }
 }
--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -32,7 +32,8 @@ export default {
         const responseData = await response.json();
 
         if (!response.ok) {
-            //error
+            const error = new error(responseData.message || 'failed to fetch!');
+            throw error;
         }
 
         const coaches = [];
