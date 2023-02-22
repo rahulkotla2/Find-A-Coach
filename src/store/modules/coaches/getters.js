3b44ac8e3528bce604e1be00132b348230946778
rahulkotla2 <rahulkotla2@gmail.com>
2023-02-22 14:42:12 +0530

    Caching Http response data

--- src/store/modules/coaches/getters.js
+++ src/store/modules/coaches/getters.js
@@ -5,9 +5,17 @@ export default {
     hasCoaches(state) {
         return state.coaches && state.coaches.length > 0;
     },
-    isCoach(state,getters,rootState,rootGetters){
-       const coaches = rootGetters['coaches/coaches'];
-       const userId = rootState.userId;
-       return coaches.some(coach => coach.id === userId);
+    isCoach(state, getters, rootState, rootGetters) {
+        const coaches = rootGetters['coaches/coaches'];
+        const userId = rootState.userId;
+        return coaches.some(coach => coach.id === userId);
+    },
+    shouldUpdate(state) {
+        const lastFetch = state.lastFetch;
+        if (!lastFetch) {
+            return true;
+        }
+        const currentTimeStamp = new Date().getTime();
+        return (lastFetch - currentTimeStamp) / 1000 > 60;
     }
 };
