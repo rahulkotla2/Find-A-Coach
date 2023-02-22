d5d815028ce927425b49253b92adc6421c8f132e
rahulkotla2 <rahulkotla2@gmail.com>
2023-02-22 15:12:12 +0530

    Added NOt Found Component

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,10 @@
 <template>
-    NOT FOUND
+    <section>
+        <base-card>
+            <h2>Page Not Found</h2>
+            <p>This Page could not be found - may check out all our
+                <router-link to="/coaches">Coaches</router-link>
+            </p>
+        </base-card>
+    </section>
 </template>
