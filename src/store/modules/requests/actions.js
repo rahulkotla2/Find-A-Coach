9e16f6368bb2a6c0a074491b6d394bdd46fbf8c7
rahulkotla2 <rahulkotla2@gmail.com>
2023-02-22 10:33:58 +0530

    Added post request for Requests action

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,23 @@
 export default {
-    contactCoach(context, payload) {
+    async contactCoach(context, payload) {
         const newRequest = {
-            id: new Date().toISOString(),
-            coachId: payload.coachId,
             userEmail: payload.email,
             message: payload.message,
         }
+
+        const response = await fetch(`https://find-a-coach-418ac-default-rtdb.firebaseio.com/${payload.coachId}.json`, {
+            method: 'POST',
+            body: JSON.stringify(newRequest),
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            const error = new Error(responseData.message || 'Failed to send request');
+        }
+
+
+        newRequest.id = responseData.name;
         context.commit('addRequest', newRequest);
     },
 }
