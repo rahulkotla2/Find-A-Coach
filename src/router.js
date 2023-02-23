329667d484732d38b556a2d010d5b9029cada909
rahulkotla2 <rahulkotla2@gmail.com>
2023-02-23 18:06:04 +0530

    Authentication and Routing

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
 import NotFound from './pages/NotFound.vue';
+import store from './store/index.js';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -16,12 +17,22 @@ const router = createRouter({
             path: '/coaches/:id', component: CoachDetail, props: true,
             children: [{ path: 'contact', component: ContactCoach }]
         },
-        { path: '/register', component: CoachRegistration },
-        { path: '/auth', component: UserAuth },
-        { path: '/requests', component: RequestsReceived },
+        { path: '/register', component: CoachRegistration, meta: { requiresAuth: true } },
+        { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
+        { path: '/requests', component: RequestsReceived, meta: { requiresAuth: true } },
         { path: '/:notFound(.*)', component: NotFound }
 
     ]
+});
+
+router.beforeEach(function (to, _, next) {
+    if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+        next('/auth');
+    } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+        next('/coaches');
+    } else {
+        next();
+    }
 });
 
 export default router;
