dca734ebf858d87df55b239ff6427655a6b02549
rahulkotla2 <rahulkotla2@gmail.com>
2023-02-23 11:42:19 +0530

    Preparing Vuex

---
+++ src/store/modules/auth/index.js
@@ -0,0 +1,14 @@
+import getters from './getters.js';
+import mutations from './mutations.js';
+import actions from './actions.js';
+
+export default {
+    state() {
+        return {
+            userId: 'c3',
+        }
+    },
+    mutations,
+    actions,
+    getters,
+};
