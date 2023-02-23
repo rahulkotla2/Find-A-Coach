da44f027dd77af13dc1f64c4dbf1e86b6168feec
rahulkotla2 <rahulkotla2@gmail.com>
2023-02-23 16:39:15 +0530

    Added logout action and flow

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -8,6 +8,7 @@
                 <li v-else>
                     <router-link to="/auth">Login</router-link>
                 </li>
+                <li v-if="isLoggedIn"><base-button @click="logout">Logout</base-button></li>
             </ul>
         </nav>
     </header>
@@ -18,6 +19,11 @@ export default {
     computed: {
         isLoggedIn() {
             return this.$store.getters.isAuthenticated;
+        },
+    },
+    methods: {
+        logout() {
+            this.$store.dispatch('logout');
         }
     }
 }
