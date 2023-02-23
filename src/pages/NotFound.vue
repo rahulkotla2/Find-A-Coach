5531ae29a927d6fe01c1055098253b38b9106a6c
rahulkotla2 <rahulkotla2@gmail.com>
2023-02-23 11:22:06 +0530

    Added the Authentication page (Login and Signup)

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -2,7 +2,7 @@
     <section>
         <base-card>
             <h2>Page Not Found</h2>
-            <p>This Page could not be found - may check out all our
+            <p>This Page could not be found - may be check out all our
                 <router-link to="/coaches">Coaches</router-link>
             </p>
         </base-card>
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
@@ -16,6 +17,7 @@ const router = createRouter({
             children: [{ path: 'contact', component: ContactCoach }]
         },
         { path: '/register', component: CoachRegistration },
+        { path: '/auth', component: UserAuth },
         { path: '/requests', component: RequestsReceived },
         { path: '/:notFound(.*)', component: NotFound }
 
